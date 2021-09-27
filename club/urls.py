aa051aad3b2a3fb6c364dc08743446b8a2fd8efe
Taurus97123 <taurus.simon71@gmail.com>
2021-09-27 15:15:39 +0200

    from server setting static

--- club/urls.py
+++ club/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from club import views
 from django.conf import settings
-from django.conf.urls.static import static
+"""from django.conf.urls.static import static"""
 
 
 app_name = 'club'
@@ -14,5 +14,5 @@ urlpatterns = [
     path('supprimer-profil/', views.deleteprofil, name='deleteprofil'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+"""if settings.DEBUG:"""
+"""urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)"""
